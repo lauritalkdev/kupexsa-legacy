2d3f5c4c8b8c3a26c45c505464d235edf9a51d1c
lauritalkdev <lauritalkdev@gmail.com>
2026-07-21 02:14:12 +0100

    Setup KUPEXSA design foundation

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const APP_NAME = "KUPEXSA Connect";
+
+export const PROJECT_NAME = "KUPEXSA Legacy";
+
+export const SCHOOL_NAME =
+  "Presbyterian High School (PHS), Kumba";
+
+export const SCHOOL_MOTTO =
+  "Servizium, Scientia, Integritas";
+
+export const KUPEXSA_SLOGAN =
+  "Proud to Belong";
+
+export const FOUNDATION_YEAR = 1963;
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Member {
+  id: string;
+  fullName: string;
+  entryYear?: number;
+  graduationYear?: number;
+  badge?: string;
+  country?: string;
+  chapter?: string;
+  occupation?: string;
+  profileImage?: string;
+}
